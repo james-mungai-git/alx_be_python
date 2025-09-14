5f3db83a5c181bbd969f8030a4dbef82a2896b0c
james-mungai-git <j.murigi.mungai@gmail.com>
2025-09-14 08:16:43 +0300

    made changes to finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,8 @@
-# finance_calculator.py
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = int(monthly_income - monthly_expenses)
 
-# Define variables
-monthly_income = 3000
-monthly_expenses = 2000
-
-# Calculate monthly savings
-monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are $ {monthly_savings}")
 
 # Define interest rate
 interest_rate = 0.05
