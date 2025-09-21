569118b969292caf1808ee89825272e80d8f915c
james-mungai-git <j.murigi.mungai@gmail.com>
2025-09-21 21:18:14 +0300

    Add weather files

---
+++ control-flow/weather_text.py
@@ -0,0 +1,14 @@
+weather =input("what is the weather like today? (sunny/rainy/cold) ")
+print("message")
+
+if weather =="sunny":
+  print("Wear a t-shirt and sunglasses.")
+
+elif weather == "rainy":
+  print("Don't forget your umbrella and a raincoat.")
+
+elif weather == "cold":
+  print("Make sure to wear a warm coat and a scarf.")
+
+else:
+  print("Sorry, I don't have recommendations for this weather.")
