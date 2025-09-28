9a6608f0e7ba4ae78c5e560849511db80ca60988
james-mungai-git <j.murigi.mungai@gmail.com>
2025-09-28 21:50:53 +0300

    add arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+def perform_operation ():
+  numb1 = float(input("Enter the first number: "))
+  numb2 = float(input("Enter the second number: "))
+
+
+  operation = input("enter the operation (add, subtract, multiply, divide): ").lower()
+   
+  match operation:
+   
+   case "add":
+     print(f"Result: {numb1 + numb2} ")
+   
+   case "subtract":
+     print(f"Result: {numb1 - numb2}" )
+
+   case "multiply":
+     print(f"Result: {numb1 * numb2}" )
+
+   case "divide":
+            if numb2 == 0:
+                return "Error: Division by zero!"
+            else:
+                return numb1 / numb2
+
+perform_operation()
+  
+   
+ 
